d4db044ce44c599230ebf2128871578b83f8665e
JYS <dbstjs1452@gmail.com>
2022-09-11 14:26:00 +0900

    first commit by JYS

---
+++ A.py
@@ -0,0 +1,5 @@
+num=1
+str='Hello Git!'
+
+print(str)
+print("I am JYS")
---
+++ B.py
@@ -0,0 +1,7 @@
+a = ["a","b","c","d","e","f","a"]
+print(a[5])
+print(a[-2])
+print(a[1:3])
+print(a[3:])
+print(a[:3])
+print(a[-3:-1]) 
