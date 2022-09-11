44e6e90d22d039f7b9b23342eadf830c5dc00974
JYS <dbstjs1452@gmail.com>
2022-09-11 16:53:36 +0900

    after new branch

--- A.py
+++ A.py
@@ -6,3 +6,14 @@ for x in range(1, 20):
 for x in range(1, 20):
     print(x)
     print("1231321231231123")
+print(str)
+for x in range(1, 11):
+    print("I am JYS", x)
+    print("22222222-by ys")
+for x in range(1, 11):
+    print("I am Yuhyun",x)
+    print("33333333-by ys!")
+for x in range(1, 10):
+    print("Hello, I am JYS", x)
+for x in range(1, 10):
+    print("Hello, I am OYH!",x)
