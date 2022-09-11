3ada0cb3071743b7f088de898ae11bfaab8e94a1
JYS <dbstjs1452@gmail.com>
2022-09-11 15:15:49 +0900

    5 commit JYS

--- A.py
+++ A.py
@@ -1,14 +1,8 @@
 num=11212112
 str='Hello Git!!!!!!!!!!!!!!!!!'
 
-print(str)
-for x in range(1, 11):
-    print("I am JYS", x)
-    print("22222222-by ys")
-for x in range(1, 11):
-    print("I am Yuhyun",x)
-    print("33333333-by ys")
 for x in range(1, 20):
     print("Hello, I am JYS", x)
 for x in range(1, 20):
-    print("Hello, I am OYH",x)
+    print(x)
+    print("1231321231231123")
