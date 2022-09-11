bf1a6e58baa78e6182181a1b8589def90eeb9f42
JYS <dbstjs1452@gmail.com>
2022-09-11 16:56:16 +0900

    push

--- A.py
+++ A.py
@@ -1,8 +1,4 @@
 num=11212112
 str='Hello Git!!!!!!!!!!!!!!!!!'
 
-for x in range(1, 20):
-    print("Hello, I am JYS", x)
-for x in range(1, 20):
-    print(x)
-    print("1231321231231123")
+print("delete")
--- B.py
+++ B.py
@@ -5,3 +5,5 @@ print(a[1:3])
 print(a[3:])
 print(a[:3])
 print(a[-3:-1]) 
+
+print("add")
