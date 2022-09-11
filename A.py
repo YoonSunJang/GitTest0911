0fb64d18b673962d963b934504b74031b4e541a8
oyh <sksms45120@gmail.com>
2022-09-11 15:14:35 +0900

    oyh이 master 수정했다

--- A.py
+++ A.py
@@ -7,8 +7,8 @@ for x in range(1, 11):
     print("22222222-by ys")
 for x in range(1, 11):
     print("I am Yuhyun",x)
-    print("33333333-by ys")
-for x in range(1, 20):
+    print("33333333-by ys!")
+for x in range(1, 10):
     print("Hello, I am JYS", x)
-for x in range(1, 20):
-    print("Hello, I am OYH",x)
+for x in range(1, 10):
+    print("Hello, I am OYH!",x)
