72cdd47741d42f07e86d2d1d85b70c4522e90599
oyh <sksms45120@gmail.com>
2022-09-11 15:06:22 +0900

    again

--- A.py
+++ A.py
@@ -2,7 +2,7 @@ num=11212112
 str='Hello Git!!!!!!!!!!!!!!!!!'
 
 print(str)
-for x in range(1, 11):
-    print("I am JYS", x)
-for x in range(1, 11):
-    print("I am OYH",x)
+for x in range(1, 20):
+    print("Hello, I am JYS", x)
+for x in range(1, 20):
+    print("Hello, I am OYH",x)
