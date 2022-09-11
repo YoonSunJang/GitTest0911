8f307037940bae09cffba16f19e5dd63916b12e2
JYS <dbstjs1452@gmail.com>
2022-09-11 15:07:43 +0900

    Thrid commit JYS

--- A.py
+++ A.py
@@ -4,5 +4,7 @@ str='Hello Git!!!!!!!!!!!!!!!!!'
 print(str)
 for x in range(1, 11):
     print("I am JYS", x)
+    print("22222222-by ys")
 for x in range(1, 11):
     print("I am Yuhyun",x)
+    print("33333333-by ys")
